b500a1ebe8873388e6768685b04d92e66a1ad324
Juan Restrepo <juan.restrepo13@u.icesi.edu.co>
2023-05-20 21:28:44 -0500

    add space

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const util = require('util')
 
 //var initDatabase = require('./scripts/init_database');
 var bootstrapDB = require('./scripts/bootstrapDB.js');
+
 var database = require('./scripts/database.js');
 
 
